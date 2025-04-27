5c21f88831800c673615e4deb200ee3b66b4b760
likunlong <likunlong@gyyx.cn>
2025-04-27 16:54:45 +0800

    feat: 添加相关依赖；使用四种方式进行AI模型的调用；

---
+++ src/main/java/cn/longai/longaiagent/demo/invoke/LangChainAiInvoke.java
@@ -0,0 +1,16 @@
+package cn.longai.longaiagent.demo.invoke;
+
+import dev.langchain4j.community.model.dashscope.QwenChatModel;
+import dev.langchain4j.model.chat.ChatLanguageModel;
+
+public class LangChainAiInvoke {
+
+    public static void main(String[] args) {
+        ChatLanguageModel qwenModel = QwenChatModel.builder()
+                .apiKey(TestApiKey.API_KEY)
+                .modelName("qwen-max")
+                .build();
+        String answer = qwenModel.chat("我是龙猫，正在学习AIAgent的java实战项目，给我一点建议！");
+        System.out.println(answer);
+    }
+}
---
+++ src/main/java/cn/longai/longaiagent/demo/invoke/TestApiKey.java
@@ -0,0 +1,6 @@
+package cn.longai.longaiagent.demo.invoke;
+
+public interface TestApiKey {
+
+    String API_KEY = "sk-488ba1054a114c98bbdc6b594a5cef5b";
+}
