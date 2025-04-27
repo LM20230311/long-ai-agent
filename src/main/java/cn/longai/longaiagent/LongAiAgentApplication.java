798754d0f7d2a0af08ee00bd322a4fc7a3b2d0b1
likunlong <likunlong@gyyx.cn>
2025-04-27 16:18:47 +0800

    feat: 初始化项目；

---
+++ src/main/java/cn/longai/longaiagent/LongAiAgentApplication.java
@@ -0,0 +1,13 @@
+package cn.longai.longaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LongAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LongAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/longai/longaiagent/LongAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.longai.longaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LongAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
